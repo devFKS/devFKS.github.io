4d532c2b7d79b0c840a1fc6952e78fc89a0e677a
devFKS <97810571+devFKS@users.noreply.github.com>
2022-01-16 13:17:52 -0600

    r2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "fd05b154cf5a026d3fc95716f4cc0e6c",
-"index.html": "4a4f50a0f4b38992d2e191ae46fe246b",
-"/": "4a4f50a0f4b38992d2e191ae46fe246b",
-"main.dart.js": "f36f673a8978540584cf153b3e66b614",
+"index.html": "c5a733068f647f9de4129b4fa921ca65",
+"/": "c5a733068f647f9de4129b4fa921ca65",
+"main.dart.js": "fc32d1da6930d62e9c49d2770d68dedf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
